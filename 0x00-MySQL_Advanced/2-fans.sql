37e98561653cec7cb29fbe60c4f2fbaf6143e828
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-08-10 21:04:51 +0300

    Best band ever!

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,4 @@
+-- A SQL script that ranks Country origins of bands, ordered by
+-- the number of (non-unique) fans
+
+SELECT origin, SUM(fans) as nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
