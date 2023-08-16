5c99b7d7c9e5232d6fd4d8cc024c6b6d4444cd15
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-08-16 18:23:30 +0300

    Added function and corrected program flow and also documentation

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,38 +1,46 @@
 #!/usr/bin/env python3
-"""A module that displays Nginx logs stored in MongoDB"""
-
+"""
+    A module that provides stats about Nginx logs stored on MongoDB
+"""
 import pymongo
 
-# start MongoDb
-sudo service mongod start
 
-# MongoDB connection settings
-db_name = "logs"
-collection_name = "nginx"
+def log_stats():
+    """
+        This function Counts the number of documents stored in the
+        database and prints the results based on the criteria provided
+    """
+    # Get total number of documents in the collection
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
 
-# connecting to MongoDB
-client = pymongo.MongoClient()
-db = client[db_name]
-collection = db[collection_name]
+    # Get the number of documents with each HTTP method
+    http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method_stats = [collection.count_documents({"method": method})
+                    for method in http_methods]
 
-# Getting the number of documents in the collection
-total_logs = collection.count_documents({})
-print(f"{total_logs} logs")
+    print("Methods:")
+    for method, count in zip(http_methods, method_stats):
+        print(f"\tmethod {method}: {count} logs")
 
-# Get the number of documents with each HTTP method
-http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-method_stats = [collection.count_documents({"method": method})
-                for method in http_methods]
-
-print("Methods:")
-for method, count in zip(http_methods, method_stats):
-    print(f"\tmethod {method}: {count} logs")
-
-# Get the no. of documents with method=GET and path=/status
-status_path_logs = collection.count_documents(
-        {"method": "GET", "path": "/status"}
+    # Get the no. of documents with method=GET and path=/status
+    status_path_logs = collection.count_documents(
+            {"method": "GET", "path": "/status"}
     )
-print(f"{status_path_logs} status check")
+    print(f"{status_path_logs} status check")
 
-# Close the MongoDB connection
-client.close()
+
+if __name__ == "__main__":
+    # MongoDB connection settings
+    database_name = "logs"
+    collection_name = "nginx"
+
+    # Connect to MongoDB
+    client = pymongo.MongoClient()
+    db = client[database_name]
+    collection = db[collection_name]
+
+    log_stats()
+
+    # Close the connection
+    client.close()
